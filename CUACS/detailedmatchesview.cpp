e1b7e4bd6dc8577cbfa8673d956c6ee96434a5d0
davidNguyenCU <davidnguyen3@cmail.carleton.ca>
2019-04-08 02:24:26 -0400

    Verifying that detailed screen works. Displays properties of each match. Not fully implemented. Still need to fill out the rest of the attributes of each match

--- CUACS/detailedmatchesview.cpp
+++ CUACS/detailedmatchesview.cpp
@@ -44,5 +44,27 @@ void DetailedMatchesView::on_previousBtn_clicked()
 
 void DetailedMatchesView::update(){
     std::pair<Client, Animal> currentPair = matches.at(index);
-    ui->clientName->setText(currentPair.first.getFirstName());
+    ui->clientName->setText(currentPair.first.getFirstName() + " " + currentPair.first.getLastName());
+    ui->clientBehavePref->setText(QString::number(currentPair.first.getBehaviour()));
+    /*ui->clientControlPref->setText(currentPair.first.getOwnerControl());
+    ui->clientSocialPref->setText(currentPair.first.getSociability());
+
+    ui->clientChildPref->setText(currentPair.first.getChildFriendly());
+    ui->clientStrangePref->setText(currentPair.first.getStrangerFriendly());
+
+    ui->behaveRank->setText(currentPair.first.getBehaveRank());
+    ui->controlRank->setText(currentPair.first.getOwnerRank());
+    ui->socialRank->setText(currentPair.first.getSocialRank());
+
+    ui->animalName->setText(currentPair.second.getName());
+    ui->animalStrangePref->setText(currentPair.second.getStrangerFriendly());
+    ui->animalChildPref->setText(currentPair.second.getChildFriendly());
+
+    ui->trainability->setText(currentPair.second.getTrainability());
+    ui->intelligence->setText(currentPair.second.getIntelligence());
+    ui->obedience->setText(currentPair.second.getObedience());
+    QString gargantuan = currentPair.second.getTrainability() + ' ' + currentPair.second.getIntelligence() + ' ' + currentPair.second.getObedience() + ") /30 = ";
+    //(currentPair.second.getTrainability()+currentPair.second.getIntelligence()+currentPair.second.getObedience())/30.0
+    ui->controlScore->setText(gargantuan);*/
+
 }
